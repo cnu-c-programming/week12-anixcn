9dc53903b1282c312c75d88586a2bf0b5662232a
anixcn <anixcnu@gmail.com>
2026-05-28 23:04:07 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
-        return 0;
+
+    if (argc < 2) return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) return 1;
 
+    int count[26] = {0};
+    int ch;
 
+    while ((ch = fgetc(fp)) != EOF) { 
+        if (ch >= 'a' && ch <= 'z') count[ch - 'a']++;
+    }
+    for (int i = 0; i < 26; i++) 
+        printf("%c: %d\n", 'a' + i, count[i]);
 
     fclose(fp);
 }
-
+    
