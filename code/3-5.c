dd88bdf5d1de8e765f43e4e3da94ae5379ecc8a5
anixcn <anixcnu@gmail.com>
2026-05-28 23:51:56 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char* argv[]){
     const char* filename = "numbers.txt";
 
-    FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    FILE* fp = fopen(filename,"r");
+    if(fp == NULL){
         return 0;
     }
-    
-    char buffer[256];
-    int sum = 0;
 
-    while (fscanf(fp, "%255s", buffer) == 1) {
-        char* endptr;
-        
-        long value = strtol(buffer, &endptr, 10);
-        if (buffer == endptr || *endptr != '\0')  continue;
-        
-        sum += (int) value;
+    int sum = 0;
+    char buffer[256];
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        int value;
+
+        if (sscanf(buffer, "%d", &value) == 1) sum += value;
+        else  fprintf(stderr, "%s", buffer);
     }
 
-    printf("sum: %d\n", sum);
+    printf("sum: %d\n",sum);
     fclose(fp);
     return 0;
 }
