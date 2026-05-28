6c44784929c14314c93469e02d03a72208275a0e
anixcn <anixcnu@gmail.com>
2026-05-28 23:41:23 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -7,10 +7,18 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
-
-
+    
+    char buffer[256];
     int sum = 0;
 
+    while (fscanf(fp, "%255s", buffer) == 1) {
+        char* endptr;
+        
+        long value = strtol(buffer, &endptr, 10);
+        if (buffer == endptr || *endptr != '\0')  continue;
+        
+        sum += (int) value;
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
