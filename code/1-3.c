08e0a7f8e126b989135cd13df596ae35c34f8684
anixcn <anixcnu@gmail.com>
2026-05-28 22:55:39 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -2,9 +2,9 @@
 
 int main(int argc, const char* argv[], const char* envp[]) {
     
-    while (*envp != NULL) {
-        printf("%s\n", *envp);
-        envp++;
+    while (envp[i] != NULL)) {
+        printf("%s\n", envp[i]);
+        i++;
     }
 
     return 0;
